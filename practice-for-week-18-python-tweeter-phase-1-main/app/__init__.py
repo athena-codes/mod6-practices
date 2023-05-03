d466482f55ebd61f83f07b3ad89a52c9744671b9
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-05-02 20:24:25 -0400

    added a homepage route for twitter mock app

--- practice-for-week-18-python-tweeter-phase-1-main/app/__init__.py
+++ practice-for-week-18-python-tweeter-phase-1-main/app/__init__.py
@@ -1,8 +1,18 @@
 # !!START
-from flask import Flask
+from flask import Flask, render_template
 from .config import Config
+from .tweets import tweets
+from random import choice
+
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
+
+@app.route('/')
+def index():
+    tweet = choice(tweets)
+    return render_template("index.html", tweet=tweet)
+
+
 # !!END
