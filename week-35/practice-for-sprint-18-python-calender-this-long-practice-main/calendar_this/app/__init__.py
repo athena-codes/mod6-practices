5f2f3b469678f5e29283e989e95e63f310d0a2d7
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-05-04 18:50:03 -0400

    added render template

--- week-35/practice-for-sprint-18-python-calender-this-long-practice-main/calendar_this/app/__init__.py
+++ week-35/practice-for-sprint-18-python-calender-this-long-practice-main/calendar_this/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from .routes.routes import bp
 from .config import Config
 
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
+app.register_blueprint(bp)
 
-@app.route('/')
-def index():
-    return '<h1> I work :D </h1>'
+# @app.route('/')
+# def index():
+#     return '<h1> I work :D </h1>'
